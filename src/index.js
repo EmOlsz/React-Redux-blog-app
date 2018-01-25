464521b0dc8e60daed6e1900fd64142110e2cf4b
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-01-25 22:39:40 +0100

    Added example components for react router mechanism

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Entry extends Component {
+    render() {
+        return <div>Entry</div>
+    }
+}
+
+class Exit extends Component {
+    render() {
+        return <div>Exit</div>
+    }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    {/*<App />*/}
+      <BrowserRouter>
+          <div>
+              <Route path="/entry" component={Entry}/>
+              <Route path="/exit" component={Exit}/>
+          </div>
+      </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
