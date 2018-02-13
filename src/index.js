7fcccac1258c23806a6850846f9e6ee78003e0f1
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-02-13 13:41:22 +0100

    Added new route and post creator component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostIndex from './components/post_index';
+import PostNew from './components/post_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,6 +17,7 @@ ReactDOM.render(
       <BrowserRouter>
           <div>
               <Route path="/" component={PostIndex} />
+              <Route path="/posts/new" component={PostNew} />
           </div>
       </BrowserRouter>
   </Provider>
