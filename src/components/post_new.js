626d069d3390957fdb8c90d96661f8a69f724351
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-02-15 18:38:03 +0100

    Created form with three inputs, created helper to reuse code for creating each input

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
+
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <input
+          className="form-control"
+          placeholder={field.placeholderValue}
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
         <h1>New post:</h1>
+        <form>
+          <Field
+            name="title"
+            placeholderValue="Title"
+            component={this.renderField}
+          />
+          <Field
+            name="categories"
+            placeholderValue="Categories"
+            component={this.renderField}
+          />
+          <Field
+            name="content"
+            placeholderValue="Content"
+            component={this.renderField}
+          />
+        </form>
       </div>
     )
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'CreateNewPost'
+})(PostNew);
